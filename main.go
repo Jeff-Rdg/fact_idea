e26f70d8215edcdaeff50fe61bf6c7db2daa001f
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2024-03-17 15:49:13 -0300

    feat: implements new methods

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/jeff-rdg/idea_pattern/httpRequest"
 	"github.com/jeff-rdg/idea_pattern/integrationPost"
 	"github.com/jeff-rdg/idea_pattern/integrationPost/integrationRequest"
 	"github.com/jeff-rdg/idea_pattern/integrationPost/integrationResponse"
 )
 
 func main() {
+
+	res, err := httpRequest.New("https://jsonplaceholder.typicode.com/posts", httpRequest.GET).
+		SetParams(map[string]interface{}{
+			"userId": 1,
+		}).
+		Send()
+
+	fmt.Println(string(res))
+
 	post := &integrationRequest.PostRequest{
 		Title:  "Testando integração",
 		Body:   "numero 01",
